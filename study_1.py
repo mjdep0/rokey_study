87529107ff3a23db750ccef31e181b798d7137f5
dlwornjs2-creator <dlwornjs1999@gmail.com>
2026-02-26 18:22:09 +0900

    Update study_1.py

--- study_1.py
+++ study_1.py
@@ -1,14 +1,15 @@
 class Fruit:
-    def __init__(self, name, color):
+    def __init__(self, name, color, size):
         self.name = name
         self.color = color
+        self.size = size
 
     def taste(self):
         print(f"{self.name}는 새콤하다")
 
-orange = Fruit(name='오렌지', color='노란색')
-
+orange = Fruit(name='오렌지', color='노란색', size='보통')
 print(f"이름: {orange.name}")
 print(f"색상: {orange.color}")
+print(f"크기: {orange.size}")
 
 orange.taste()
