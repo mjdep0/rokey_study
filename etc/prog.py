e4c781126fe3ad5945215ca24baaa48f4078247b
junh0012 <junh001224@gmail.com>
2026-02-26 16:55:11 +0900

    Delete etc directory

--- etc/prog.py
+++
@@ -1,9 +0,0 @@
-def solution(A,B):
-    answer = 0
-    A.sort(reverse=True)
-    B.sort()
-    for i in range(len(A)):
-        answer += A[i] * B[i]
-
-    return answer
-print(solution([1,4,2], [5,4,4]))
